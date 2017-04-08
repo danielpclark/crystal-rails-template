8e3a0275a99191fb7a350b154b030c2fbcc60130
Daniel P. Clark <6ftdan@gmail.com>
2017-04-08 07:52:59 -0400

    Simplify index view example

--- app/controllers/index_controller.cr
+++ app/controllers/index_controller.cr
@@ -3,11 +3,6 @@ require "./base_controller"
 module Railslike
   module Controllers
     class IndexController < BaseController
-
-      private def request_query
-        Kemal::ParamParser.new(env.request).query
-      end
-
     end
   end
 end
